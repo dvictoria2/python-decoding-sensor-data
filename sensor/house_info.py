3f06b15fb4c35446500a5d68dce8caa4ce65dbae
dvictoria2 <dvictoria2@slb.com>
2023-01-20 10:56:45 -0500

    Update house_info.py

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -1,32 +1,22 @@
 #from load_info import load_sensor_data
 #from house_info import HouseInfo
 #from datetime import datetime, date
+from load_info import load_sensor_data
+
+# Module 3: Create an instance of sensor data
 
 
-
-
-class HomuseInfo(object):
+class HomeData(object):
     def __init__(self, data):
         self.data = data
 
-    def get_data_by_area(self, field, rec_area=0):
-        field_data = []
+    def get_data_by_room(self, field, room=0):
+        data = []
         # loop over records
         for record in self.data:
             # filter data by room
-            if rec_area == 0:                           # take all room
-                field_data.append(record[field])
-            elif rec_area == int(record['area']):       # select room
-                field_data.append(record[field])
-        return field_data
-
-        
-    def get_data_by_date(self, field, rec_area=date.today()):
-        field_data = []
-
-        # loop over records
-        for record in self.data:
-            # filter data by room
-            if rec_date.strftime("%m/%d/%y") -- record['date']                   # take all room
-            fiel.data.append(record[field])
-        return field_data
+            if room == 0:                           # take all room
+                data.append(record[field])
+            elif room == int(record['room']):       # select room
+                data.append(record[field])
+        return data
