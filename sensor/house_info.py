30b4b8e4b95a8c08fc92b0ea55119215b4b1784d
dvictoria2 <dvictoria2@slb.com>
2023-01-19 22:09:41 -0500

    Update house_info.py

---
+++ sensor/house_info.py
@@ -0,0 +1,32 @@
+#from load_info import load_sensor_data
+#from house_info import HouseInfo
+#from datetime import datetime, date
+
+
+
+
+class HomuseInfo(object):
+    def __init__(self, data):
+        self.data = data
+
+    def get_data_by_area(self, field, rec_area=0):
+        field_data = []
+        # loop over records
+        for record in self.data:
+            # filter data by room
+            if rec_area == 0:                           # take all room
+                field_data.append(record[field])
+            elif rec_area == int(record['area']):       # select room
+                field_data.append(record[field])
+        return field_data
+
+        
+    def get_data_by_date(self, field, rec_area=date.today()):
+        field_data = []
+
+        # loop over records
+        for record in self.data:
+            # filter data by room
+            if rec_date.strftime("%m/%d/%y") -- record['date']                   # take all room
+        fiel.data.append(record[ield])
+        return field_data
