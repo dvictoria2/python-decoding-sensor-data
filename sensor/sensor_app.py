daeb9574002c2c729007c57e354ff1ccac57d8ec
dvictoria2 <dvictoria2@slb.com>
2023-01-20 11:09:41 -0500

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,6 +1,7 @@
 # Runner script for all modules
-
 from load_data import load_sensor_data
+from house_info import HousInfo
+from datetime import datetime, date
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
@@ -19,9 +20,9 @@ test_area = 1
 recs = house_info.get_data_by_area("id", rec_area=test_area)
 print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
 
-test_data = datetime.stoptime ("s/9/20", "%m/%d/%y")
-recs = hpuse_info.get_data_by_date("id", rec_datev= test_data)
-print("House sensor records for date {} = {}".format(test_date.strftime("%m/%d/%y")))
+test_data = datetime.strptime("s/9/20", "%m/%d/%y")
+recs = hpuse_info.get_data_by_date("id", rec_date= test_date)
+print("House sensor records for date {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 # Module 3 code here:
 # Module 4
 print("\nProcessing Temperature Information")
