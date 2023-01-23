202c4f2f67052ac8f518bcb3b5a362dd01fbd515
dvictoria2 <dvictoria2@slb.com>
2023-01-23 13:56:31 -0500

    Update humidity_info.py

--- sensor/humidity_info.py
+++ sensor/humidity_info.py
@@ -1,10 +1,19 @@
 from house_info import HouseInfo
+<<<<<<< Updated upstream
 from datetime import date, datetime
+=======
+from datetime import date
+
+>>>>>>> Stashed changes
 
 class HumidityData(HouseInfo):
     def _convert_data(self, data):
         recs = []
         for rec in data:
+<<<<<<< Updated upstream
+=======
+            # Convert string of integers into actual integers based 10
+>>>>>>> Stashed changes
             recs.append(float(rec) * 100)
         return recs
 
@@ -15,4 +24,8 @@ class HumidityData(HouseInfo):
 
     def get_data_by_date(self, rec_date=date.today()):
         recs = super().get_data_by_date("humidity", rec_date)
+<<<<<<< Updated upstream
         return self._convert_data(recs)
+=======
+        return self._convert_data(recs)
+>>>>>>> Stashed changes
