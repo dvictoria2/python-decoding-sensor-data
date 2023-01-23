c6d7a2d1aa8aa5b0833a98c32e8156250af810b6
dvictoria2 <dvictoria2@slb.com>
2023-01-23 14:00:12 -0500

    Update energy_info.py

--- sensor/energy_info.py
+++ sensor/energy_info.py
@@ -1,14 +1,29 @@
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
 from house_info import HouseInfo
 from datetime import date
 
 
 class EnergyData(HouseInfo):
+<<<<<<< Updated upstream
 
     ENERGY_PER_BULB = 0.2        # in watts
+=======
+<<<<<<< Updated upstream
+    ENERGY_PER_BULB = 0.2
+=======
+
+    ENERGY_PER_BULB = 0.2        # in watts
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     ENERGY_BITS = 0x0F0
 
     def _get_energy(self, rec):
         energy = int(rec, base=16)
+<<<<<<< Updated upstream
         energy = energy & self.ENERGY_BITS            # mask ENERGY bits
         energy = energy >> 4                          # shift right
         return energy
@@ -17,6 +32,24 @@ class EnergyData(HouseInfo):
         recs = []
         for rec in data:
             # Convert string of hex into actual integers based 10
+=======
+<<<<<<< Updated upstream
+        energy = energy & self.ENERGY_BITS
+        energy = energy >> 4
+=======
+        energy = energy & self.ENERGY_BITS            # mask ENERGY bits
+        energy = energy >> 4                          # shift right
+>>>>>>> Stashed changes
+        return energy
+
+    def _convert_data(self, data):
+        recs = []
+        for rec in data:
+<<<<<<< Updated upstream
+=======
+            # Convert string of hex into actual integers based 10
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
             recs.append(self._get_energy(rec))
         return recs
 
@@ -30,4 +63,13 @@ class EnergyData(HouseInfo):
 
     def calculate_energy_usage(self, data):
         total_energy = sum([field * self.ENERGY_PER_BULB for field in data])
+<<<<<<< Updated upstream
         return total_energy
+=======
+<<<<<<< Updated upstream
+        return total_energy
+=======
+        return total_energy
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
