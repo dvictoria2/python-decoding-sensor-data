4a1fbc4f772274667a350e72fb7de97c71da94a0
dvictoria2 <dvictoria2@slb.com>
2023-01-24 08:54:41 -0500

    Update energy_info.py

--- sensor/energy_info.py
+++ sensor/energy_info.py
@@ -1,38 +1,29 @@
-
 from house_info import HouseInfo
 from datetime import date
 
 
 class EnergyData(HouseInfo):
-
-
     ENERGY_PER_BULB = 0.2        # in watts
-
-    ENERGY_PER_BULB = 0.2        # in watts
-
     ENERGY_BITS = 0x0F0
 
     def _get_energy(self, rec):
         energy = int(rec, base=16)
-
         energy = energy & self.ENERGY_BITS            # mask ENERGY bits
         energy = energy >> 4                          # shift right
         return energy
     
-    def _convert_data(self, data):
-        recs = []
-        for rec in data:
-            # Convert string of hex into actual integers based 10
-            energy = energy & self.ENERGY_BITS
-            energy = energy >> 4
-            energy = energy & self.ENERGY_BITS            # mask ENERGY bits
-            energy = energy >> 4                          # shift right
-        return energy
+   # def _convert_data(self, data):
+       # recs = []
+       # for rec in data:
+           # energy = energy & self.ENERGY_BITS
+           # energy = energy >> 4
+           # energy = energy & self.ENERGY_BITS            # mask ENERGY bits
+           # energy = energy >> 4                          # shift right
+       # return energy
 
     def _convert_data(self, data):
         recs = []
         for rec in data:
-            # Convert string of hex into actual integers based 10
             recs.append(self._get_energy(rec))
         return recs
 
@@ -46,8 +37,5 @@ class EnergyData(HouseInfo):
 
     def calculate_energy_usage(self, data):
         total_energy = sum([field * self.ENERGY_PER_BULB for field in data])
-
-        return total_energy
-        return total_energy
         return total_energy
 
