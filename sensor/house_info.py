6dc69d520b8d624319ebcbd30c0fc998deaa3211
dvictoria2 <dvictoria2@slb.com>
2023-01-23 20:58:40 -0500

    Update house_info.py

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -3,7 +3,7 @@ from datetime import date
 class HouseInfo(object):
     def __init__(self, data):
         self.data = data
-
+        
     def get_data_by_area(self, field, rec_area=0):
         field_data = []
         for record in self.data:
@@ -12,12 +12,11 @@ class HouseInfo(object):
             elif rec_area == int(record['area']):
                 field_data.append(record[field])
         return field_data
-
+    
     def get_data_by_date(self, field, rec_date=date.today()):
         field_data = []
-
         for record in self.data:
-            if rec_date.strftime("%m/%d/%y") == record['date']:
+            if rec_date.strftime("%m/%d/%y") == record['date']: 
                 field_data.append(record[field])
         return field_data
 
