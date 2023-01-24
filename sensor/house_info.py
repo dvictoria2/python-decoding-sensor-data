836a09c8a0ffd58a407edebfa1aace0c1be19849
dvictoria2 <dvictoria2@slb.com>
2023-01-23 20:37:50 -0500

    Update house_info.py

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -1,23 +1,19 @@
-from load_info import load_sensor_data
-#from house_info import HouseInfo
-#from datetime import datetime, date
+from datetime import date
 
-# Module 3: Create an instance of sensor data
-
-class HousInfo(object):
+class HouseInfo(object):
     def __init__(self, data):
         self.data = data
-        
-    def get_data_by_area (self, field, rec_area=0):
+
+    def get_data_by_area(self, field, rec_area=0):
         field_data = []
         for record in self.data:
             if rec_area == 0:
-              field.data.append(record[field])
-            elif rec_area == int((record['area'])):
-                field.data.append(record[field])
+                field_data.append(record[field])
+            elif rec_area == int(record['area']):
+                field_data.append(record[field])
         return field_data
 
-    def get_data_by_date (self, field, rec_date =dare.today()):
+    def get_data_by_date(self, field, rec_date=date.today()):
         field_data = []
 
         for record in self.data:
