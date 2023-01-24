8db96c26055abe02f2f9ab9c52edb21e8f0f894b
dvictoria2 <dvictoria2@slb.com>
2023-01-23 20:44:51 -0500

    Update particle_count_info.py

--- sensor/particle_count_info.py
+++ sensor/particle_count_info.py
@@ -1,26 +1,32 @@
 from house_info import HomeData
+from datetime import date
 
 # Module 6: Work with scientific notation data from Particle counter sensor
 
 
-class ParticleData(HomeData):
-    def get_data(self, field, room=0):
-        field_info = self.get_data_by_room(field, room)
-        data = []
-        for rec in field_info:
-            # Convert string of integers into floats
-            data.append(float(rec))
-        return data
+class ParticleData(HouseInfo):
+    def _convert_data(self, data):
+        recs = []
+        for rec in data:
+            recs.append(float(rec))
+        return recs
 
-    def get_concentrations(self, data):
+    def get_data_by_area(self, rec_area=0):
+        recs = super().get_data_by_area("particulate", rec_area)
+        return self._convert_data(recs)
+
+    def get_data_by_date(self, rec_date=date.today()):
+        recs = super().get_data_by_date("particulate", rec_date)
+        return self._convert_data(recs)
+
+    def get_data_concentrations(self, data):
         particulate = {"good": 0, "moderate": 0, "bad": 0}
         for rec in data:
-            # Select particulate concentration
-            if rec <= 50.0:
+             if rec <= 50.0:
                 particulate["good"] += 1
-            elif rec > 50.0 and rec <= 100:
+             elif rec > 50.0 and rec <= 100:
                 particulate["moderate"] += 1
-            else:
+             else:
                 particulate["bad"] += 1
 
         return particulate
